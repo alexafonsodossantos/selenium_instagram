1eb7d56611dc396e8c220cc28909fc489c28a68a
alexafonsodossantos <=>
2020-11-04 02:19:00 -0300

    Following and Liking scripts as functions, user and password stored on txt

--- InstaBot.py
+++ InstaBot.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from assets import Skip, Login, FollowTrending, LikeTrending
 
+f = open("pass.txt", "r")
 
-instagram_username = "thebluesbassman"
-instagram_password = "Ab742853964"
+instagram_username = f.readline()
+instagram_password = f.readline()
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
@@ -15,40 +17,11 @@ driver.get("https://instagram.com")
 
 time.sleep(5)
 
-searchUsername = driver.find_element_by_name('username')
-searchPassword = driver.find_element_by_name('password')
 
-searchUsername.send_keys(instagram_username)
-searchPassword.send_keys(instagram_password)
+Login(driver, instagram_username, instagram_password)
 
-searchPassword.send_keys(Keys.RETURN)
+Skip(driver)
 
-time.sleep(5)
-skipSave = driver.find_element_by_xpath("//*[contains(text(), 'Agora não')]").click()
+FollowTrending(driver, "pythonprogramming", 2)
 
-time.sleep(5)
-skipNotifications = driver.find_element_by_xpath("//*[contains(text(), 'Agora não')]").click()
-
-hashtag = "coding"
-
-driver.get("https://www.instagram.com/explore/tags/"+hashtag+"/")
-time.sleep(5)
-
-firstPost = driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div").click()
-time.sleep(2)
-
-firstLike = driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button").click()
-time.sleep(2)
-firstNext = driver.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a").click()
-time.sleep(2)
-
-maxLikes = 10
-
-i = 0
-while i < maxLikes:
-    time.sleep(4)
-    nextPage = driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button").click()
-    time.sleep(3)
-    nextLike = driver.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a[2]").click()
-    i = i + 1
-
+LikeTrending(driver, "Dev", 2)
--- assets.py
+++ assets.py
@@ -2,11 +2,63 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-def login(driver, username, password):
+
+def Login(driver, username, password):
     searchUsername = driver.find_element_by_name('username')
     searchPassword = driver.find_element_by_name('password')
-
     searchUsername.send_keys(username)
     searchPassword.send_keys(password)
-
     searchPassword.send_keys(Keys.RETURN)
+    time.sleep(5)
+
+def Skip(driver):
+    skipSave = driver.find_element_by_xpath("//*[contains(text(), 'Agora não')]").click()
+    time.sleep(5)
+    skipNotifications = driver.find_element_by_xpath("//*[contains(text(), 'Agora não')]").click()
+
+
+
+def FollowTrending(driver, hashtag, maxFollows):
+
+
+    driver.get("https://www.instagram.com/explore/tags/"+hashtag+"/")
+    time.sleep(5)
+
+    firstPost = driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div").click()
+    time.sleep(4)
+
+    followFirst = driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/header/div[2]/div[1]/div[2]/button").click()
+    firstNext = driver.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a").click()
+    time.sleep(2)
+
+
+    i = 0
+    while i < maxFollows:
+        time.sleep(4)
+        nextPage = driver.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a[2]").click()
+        time.sleep(3)
+        nextFollow = driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/header/div[2]/div[1]/div[2]/button").click()
+        i = i + 1
+
+
+
+
+def LikeTrending(driver, hashtag, maxLikes):
+    driver.get("https://www.instagram.com/explore/tags/"+hashtag+"/")
+    time.sleep(5)
+
+    firstPost = driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div").click()
+    time.sleep(2)
+
+    firstLike = driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button").click()
+    time.sleep(2)
+    firstNext = driver.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a").click()
+    time.sleep(2)
+
+    i = 0
+    while i < maxLikes:
+        time.sleep(4)
+        nextPage = driver.find_element_by_xpath("/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button").click()
+        time.sleep(3)
+        nextLike = driver.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a[2]").click()
+        i = i + 1
